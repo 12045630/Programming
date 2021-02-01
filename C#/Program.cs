ffb655062383ed975450f1a168f127a45d3f3b33
12045630 <76881800+12045630@users.noreply.github.com>
2021-02-01 10:48:19 +0200

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw01._02
+{
+    class Program
+    {
+        Random rnd;
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Beep();
+            int a = rnd.Next(1, 50);
+            Console.WriteLine(a);
+            string text = "";
+            if (a >= 1 && a <= 10)
+            { text = "odin"; }
+            else if (a >= 11 && a <= 20)
+            { text = "dva"; }
+            else if (a >= 21 && a <= 30)
+            { text = "tri"; }
+            else if (a >= 31 && a <= 40)
+            { text = "chetqre"; }
+            else if (a >= 41 && a <= 50)
+            { text = "pjat"; }
+            Console.WriteLine(text);
+
+            StreamWriter file = new StreamWriter(@"..\..\text.txt", true);
+            file.WriteLine(text);
+            file.Close();
+            StreamReader filest = new StreamReader(@"..\..\text.txt");
+            text = filest.ReadToEnd();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor =ConsoleColor.Yellow;
+            Console.SetCursorPosition(20,5);
+            Console.WriteLine("Failis on text: \n {0}", text);
+            
+            ConsoleKeyInfo nupp = new ConsoleKeyInfo();
+            do
+            {
+                Console.WriteLine("Vajuta Backspace...");
+                nupp = Console.ReadKey();
+
+                
+            } while (nupp.Key!=ConsoleKey.Backspace);
+
+            Console.ReadLine();
+        }
+    }
+}
